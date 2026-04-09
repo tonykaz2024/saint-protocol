044620c2496f69f34723587eeb5a1622556a324f
Auto-Claude Agent <autoclaude@local.dev>
2026-04-09 06:55:37 -0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/saint-protocol/sw.js', { scope: '/saint-protocol/' })})}
